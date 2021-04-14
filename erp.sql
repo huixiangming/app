58577d7108fa09016910dd64576eb799d2533c6b
回向明 <33412599+huixiangming@users.noreply.github.com>
2021-04-14 21:13:40 +0800

    Create erp.sql

---
+++ erp.sql
@@ -0,0 +1,193 @@
+﻿# Host: localhost  (Version: 5.7.26)
+# Date: 2021-04-14 21:13:11
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "addpurchase"
+#
+
+DROP TABLE IF EXISTS `addpurchase`;
+CREATE TABLE `addpurchase` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `supplier` varchar(255) NOT NULL DEFAULT '',
+  `date` varchar(255) NOT NULL DEFAULT '',
+  `number` varchar(255) NOT NULL DEFAULT '',
+  `sum` int(11) NOT NULL DEFAULT '0',
+  `suma` int(11) NOT NULL DEFAULT '0',
+  `sumb` int(11) NOT NULL DEFAULT '0',
+  `pay` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "addpurchasegoods"
+#
+
+DROP TABLE IF EXISTS `addpurchasegoods`;
+CREATE TABLE `addpurchasegoods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `fid` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `snumber` varchar(255) NOT NULL DEFAULT '',
+  `specs` varchar(255) NOT NULL DEFAULT '',
+  `family` varchar(255) NOT NULL DEFAULT '',
+  `unit` varchar(255) NOT NULL DEFAULT '',
+  `warehouses` varchar(255) NOT NULL DEFAULT '',
+  `addnumbers` varchar(255) NOT NULL DEFAULT '',
+  `prices` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "customer"
+#
+
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `address` varchar(255) NOT NULL DEFAULT '',
+  `phone` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=47 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "goods"
+#
+
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `snumber` int(11) NOT NULL DEFAULT '0',
+  `specs` varchar(11) NOT NULL DEFAULT '',
+  `family` varchar(255) NOT NULL DEFAULT '',
+  `unit` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "goodsclass"
+#
+
+DROP TABLE IF EXISTS `goodsclass`;
+CREATE TABLE `goodsclass` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `class` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "goodsspecs"
+#
+
+DROP TABLE IF EXISTS `goodsspecs`;
+CREATE TABLE `goodsspecs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `specs` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "goodsunit"
+#
+
+DROP TABLE IF EXISTS `goodsunit`;
+CREATE TABLE `goodsunit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `unit` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "pay"
+#
+
+DROP TABLE IF EXISTS `pay`;
+CREATE TABLE `pay` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `money` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='结算账户';
+
+#
+# Structure for table "staff"
+#
+
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `post` varchar(255) NOT NULL DEFAULT '',
+  `phone` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "stock"
+#
+
+DROP TABLE IF EXISTS `stock`;
+CREATE TABLE `stock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `goodsid` int(11) NOT NULL DEFAULT '0',
+  `warehouse` varchar(255) NOT NULL DEFAULT '',
+  `family` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `snumber` varchar(255) NOT NULL DEFAULT '',
+  `specs` varchar(255) NOT NULL DEFAULT '',
+  `number` varchar(255) NOT NULL DEFAULT '',
+  `unit` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "supplier"
+#
+
+DROP TABLE IF EXISTS `supplier`;
+CREATE TABLE `supplier` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `address` varchar(255) NOT NULL DEFAULT '',
+  `phone` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "warehouse"
+#
+
+DROP TABLE IF EXISTS `warehouse`;
+CREATE TABLE `warehouse` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) NOT NULL DEFAULT '1',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
